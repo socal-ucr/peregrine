53527134cadafab5a7c77d896b82865f7b5e482f
Kiran Ranganath <kiran7193@gmail.com>
2020-10-16 23:20:29 -0700

    Add support for all possible patterns.

--- minePattern.cc
+++ minePattern.cc
@@ -19,37 +19,46 @@ SmallGraph generateCyclic(uint32_t size)
     edge_list.emplace_back(prev, curr);
   }
   edge_list.emplace_back(size, init);
-  for (auto &[i, j] : edge_list)
-  {
-    std::cout << i << j << std::endl;
-  }
   return SmallGraph(edge_list);
+}
+
+template <typename T>
+void printResults(T results)
+{
+  for (const auto &[pattern, val] : results)
+    {
+      std::cout << pattern << " " << val << std::endl;
+    }
+
 }
 
 int main(int argc, char **argv)
 {
-  if (argc < 3)
+  if (argc < 2)
   {
-    std::cout << argv[0] << " topo.txt pattern_size\n";
+    std::cout << argv[0] << " pattern_size\n";
     exit(0);
   }
 
   else
   {
-    std::string fileName = argv[1];
-    int patternSize = atoi(argv[2]);
+    int patternSize = atoi(argv[1]);
     std::vector<Peregrine::SmallGraph> patternsToMine; // App graph
 
     patternsToMine.emplace_back(generateCyclic(patternSize)); // 4 -> 1 -> 2 -> 3 -> 4
 
-    size_t nthreads = 1;
-    const auto callback = [](auto &&handle, auto &&match) { handle.map(match.pattern, 1); };
-    auto results = match<Pattern, uint64_t, AT_THE_END, UNSTOPPABLE>(SmallGraph("topoGraph.txt"), patternsToMine, nthreads, callback);
+    std::cout << "\nRunning Match test\n" << std::endl;
 
-    for (const auto &[pattern, tmp] : results)
-    {
-      std::cout << pattern << std::endl;
-    }
+    size_t nthreads = 1;
+    // const auto callback = [](auto &&handle, auto &&match) { handle.map(match.pattern, 1); };
+    const auto callback = [](auto &&handle, auto &&match) { utils::print_vector(match.mapping); };
+    auto results = match<Pattern, uint64_t, ON_THE_FLY, UNSTOPPABLE>(SmallGraph("topoGraph.txt"), patternsToMine, nthreads, callback);
+    printResults(results);
+
+    std::cout << "\nRunning Counts test\n" << std::endl;
+
+    auto counts = count(SmallGraph("topoGraph.txt"), patternsToMine, nthreads);
+    printResults(counts);
   }
 
   return 0;
